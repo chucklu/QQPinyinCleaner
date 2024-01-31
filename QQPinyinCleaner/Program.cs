757de6d0fe86ef30626b9b59de1797461f4e0212
Chuck LU <chucklu2015@outlook.com>
2024-01-31 14:39:51 +0800

    initialize  the project

---
+++ QQPinyinCleaner/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QQPinyinCleaner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"C:\Your\Folder\Path";
+
+            try
+            {
+                // Get all files in the folder
+                string[] allFiles = Directory.GetFiles(folderPath);
+
+                // Filter files without extensions using LINQ
+                var filesWithExtensions = allFiles.Where(file => Path.GetExtension(file) != string.Empty);
+
+                // Display the filtered files
+                Console.WriteLine("Files with extensions:");
+                foreach (var file in filesWithExtensions)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
